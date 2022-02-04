34292b80c77d10a590b1475f176a8b463cfa739b
abhay-ag <abhay0034.be21@chitkara.edu.in>
2022-02-04 21:06:33 +0530

    1

--- script.js
+++ script.js
@@ -1,12 +1,3 @@
-function verify(){
-    var password = document.getElementById("pass").value
-    if(password == 5356){
-        document.getElementById("lessgo").innerHTML = ''
-    }
-    else{
-        document.getElementById("wr").style.visibility = 'visible'
-    }
-}
 function findit(){
     window.find(document.getElementById("search").value)
 }
